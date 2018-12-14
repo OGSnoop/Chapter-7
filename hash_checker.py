b929fc4c7d1adfe2745103f6fadc01b9ac5e21f4
OGSnoop <43729699+OGSnoop@users.noreply.github.com>
2018-12-13 22:04:40 -0600

    Update hash_checker.py

--- hash_checker.py
+++ hash_checker.py
@@ -4,10 +4,6 @@ import os
 import os.path
 import hashlib
 import subprocess
-from Crypto.Util.Padding import pad
-from Crypto.Util.Padding import unpad
-from base64 import b64encode
-from base64 import b64decode
 
 
 file=''
@@ -70,10 +66,10 @@ def org_dirdic():
                         perm = subprocess.check_output(['ls', '-l', dirc])
                         #hashing output from the commands
                         dir_hash = hashlib.sha1(perm).hexdigest()
-                        print(dir_hash)
+                        org_dir_dic[dirc]=dir_hash
                     except:
                         print('Improper path')
-                    org_dir_dic[dirc]=dir_hash
+                    
                 elif contin == 'n':
                     contin = 'n'
                 else:
@@ -93,9 +89,10 @@ def org_filehashdic():
                                 perm = subprocess.check_output(['ls', '-l', file])
                                 #hashing output from the commands
                                 file_hash = hashlib.sha1(perm).hexdigest()
+                                org_file_dic[file]=file_hash
                         except:
                                 print('Improper path')
-                        org_file_dic[file]=file_hash
+                        
                 elif contin == 'n':
                         contin == 'n'
                 else:
@@ -173,7 +170,6 @@ def comp_all_dir():
         global dir_hash_dic, org_dir_dic
         dirp = open('/root/Documents/dirp.txt', 'r')
         dirp_lines = dirp.readlines()
-        print(dirp_lines)
         dirp.close()
         dirlist = []
         for i in range(len(dirp_lines)):
@@ -222,7 +218,8 @@ def comp_all_file():
                         print('File %s permissions have been altered.\n' % i)
 
 #This is the key that will be used to encrypt and decrypt throughout the program
-"""
+#This part of the code would not work properly for odd reasons
+
 def key_gen():
         global key
         contin = 'true'
@@ -239,48 +236,62 @@ def key_gen():
                         print('Key not long enough')
         
 """
-key = b'fishcakes!@PARIS'
-print(type(key))
-print(key)
-
+#This is the default key I set up in the code with some content below for information display
+#with the issue I am having
+key = b'fishcakes!@PARIS'               #######COMMENT########
+print(type(key))                        #######COMMENT########
+print(key)                              #######COMMENT########
+"""
+#Here is my class for this assignment organizing encryption part of the program away from the rest
 class Dencrypt():
         def __init__(self, key):
                 self.key=key
-                #print(type(key))
-                #print(key)
+                print(type(key))               ##########UNCOMMENT#####
+                print(key)                     ########UNCOMMENT#####
 
-        def pad(self, s):
-                return s + b'\0' * (AES.block_size - len(s) % AES.block_size)
+#The pad is what extends blocks of text that are not long enough for proper encryption
+#There is a function now for this in Pycryptodome but it was easier for me to understand
+#       the encryption process by making my own
+        def pad(self, text):
+                #b'\0'  basicaly is additional empty byte segments added for encryption
+                #AES.block_size is the default 16 byte required block size for encryption
+                return text + b'\0' * (AES.block_size - len(text) % AES.block_size)
 
+#This function is the initial creation of the cipher, iv, encryption, and connection
         def encrypt_setup(self, message, key):
-                message = self.pad(message)
-                iv = Random.new().read(AES.block_size)
-                cipher = AES.new(key, AES.MODE_CBC, iv)
-                return iv + cipher.encrypt(message)
+                message = self.pad(message)             #this calls pad for additional bytes if needed
+                iv = Random.new().read(AES.block_size)  #creates random 16 byte number for the iv
+        #MODE_CBC acknowledges Cipher Block Chaining which is the 16 bytes
+                cipher = AES.new(key, AES.MODE_CBC, iv) #creates the cipher with the iv and key
+                return iv + cipher.encrypt(message)     #encrypts the block with the cipher and adds
+                                                        #the iv for complexing the encryption
 
+#This function is the initial creation of the cipher, iv, decryption, and striping
         def decrypt_setup(self, ciphertext, key):
-                iv = ciphertext[:AES.block_size]
-                cipher = AES.new(key, AES.MODE_CBC, iv)
-                text = cipher.decrypt(ciphertext[AES.block_size:])
-                return text.rstrip(b'\0')
+                iv = ciphertext[:AES.block_size]        #recreats random 16 byte number for the iv
+                cipher = AES.new(key, AES.MODE_CBC, iv) #recreates the cipher with the iv and key
+                text = cipher.decrypt(ciphertext[AES.block_size:])      #decrypts the ciphertext
+                return text.rstrip(b'\0')               #strips extra bytes from pad when needed
 
+#The function puts the encryted text into a new document and removes the old documents
         def encrypt(self, file):
-                pi_file = open(file, 'rb')
-                text = pi_file.read()
-                encryption = self.encrypt_setup(text, self.key)
-                pi_file = open(file + ".enc", 'wb')
-                pi_file.write(encryption)
-                os.remove(file)
+                pi_file = open(file, 'rb')      #opens a file in read byte mode
+                text = pi_file.read()           #reads
+                encryption = self.encrypt_setup(text, self.key) #runs the encrypt_setup function
+                pi_file = open(file + ".enc", 'wb')     #open a new file with .enc at the end in write byte mode
+                pi_file.write(encryption)       #writes the encrpted text to the file
+                os.remove(file)                 #removes the original documents
 
+#The funcition puts the decrypted text into a new doeuments and removes the encrypted documents
         def decrypt(self, file):
-                pi_file = open(file, 'rb')
-                ciphertext = pi_file.read()
-                decryption = self.decrypt_setup(ciphertext, self.key)
-                pi_file = open(file[:-4], 'wb')
-                pi_file.write(decryption)
-                os.remove(file)
+                pi_file = open(file, 'rb')      #opens the encrypted file in read byte mode
+                ciphertext = pi_file.read()     #reads the encrypted document
+                decryption = self.decrypt_setup(ciphertext, self.key)   #runs the decrypt_setup function
+                pi_file = open(file[:-4], 'wb') #opens a new without ".enc" in write byte mode
+                pi_file.write(decryption)       #writes the decrypted text
+                os.remove(file)                 #removes the encrypted document
 
-enc = Dencrypt(key)              
+#enc = Dencrypt(key)             ########COMMMENT########
 #main function that connects all of the other functions
 def program():
 	contin = 'true'
@@ -311,7 +322,6 @@ def program():
                                 subprocess.check_output(['rm', '-f', '/root/Documents/dirp.txt'])
                         except:
                                 print('Making a new file')
-
                         #The data stored in the dictionary will now be edited
                         org_dirdic()
                         #The newly edited dictionary is now stored back into the dirp.txt file
@@ -333,45 +343,47 @@ def program():
 			
                 elif option == 'cd':
                         try:
-                                input_dir()
-                                new_dirhashdic(dirc, dir_hash)
-                                read_dir()
-                                comp_dir()
+                                input_dir()     #choose a directory for comparison
+                                new_dirhashdic(dirc, dir_hash)  #create a new dictionary for comparison
+                                read_dir()      #reads documents with directory permissions into a dictionary
+                                comp_dir()      #compares the dictionaries
                         except:
                                 print('You need to decrypt some data first')
 			
                 elif option == 'cf':
                         try:
-                                input_file()
-                                new_filehashdic(file, file_hash)
-                                read_file()
-                                comp_file()
+                                input_file()    #choose a file for comparison
+                                new_filehashdic(file, file_hash)#create a new dictionary for comparison
+                                read_file()     #reads documents with file permissions into a dictionary
+                                comp_file()     #compares the dictionaries
                         except:
                                 print('You need to decrypt some data first')
 			
                 elif option == 'cda':
                         try:
-                                comp_all_dir()
-                                
+                                comp_all_dir() #comparies all of the known hashes with new ones
+
                         except:
                                 print('You need to decrypt some data first')
                         
                 elif option == 'cfa':
                         try:
-                                comp_all_file()
+                                comp_all_file() #comparies all of the known hashes with new ones
                         except:
                                 print('You need to decrypt some data first')
                         
                 elif option == 'k':
+                        pass                    ##########Commment######
                         key_gen()
                         
                 elif option == 'en':
+                        #Choose which file to encrypt
                         choice = input('What do you want to encrypt?\n'
                                        'Directories = d\n'
                                        'Files = f\n'
                                        ).lower()
                         if choice == 'd':
-                                enc.encrypt('/root/Documents/dirp.txt')
+                                enc.encrypt('/root/Documents/dirp.txt') #only needs to call encrypt function
 
                         elif choice == 'f':
                                 enc.encrypt('/root/Documents/firp.txt')
@@ -380,12 +392,13 @@ def program():
                                 print('Not a valid option')
 
                 elif option == 'de':
+                        #Choose which file to decrypt
                         choice = input('What do you want to decrypt?\n'
                                        'Directories = d\n'
                                        'Files = f\n'
                                        ).lower()
                         if choice == 'd':
-                                enc.decrypt('/root/Documents/dirp.txt.enc')
+                                enc.decrypt('/root/Documents/dirp.txt.enc')#only needs to call decrypt function
 
                         elif choice == 'f':
                                 enc.decrypt('/root/Documents/firp.txt.enc')
@@ -397,5 +410,7 @@ def program():
                         print('ERROR: Not a valid option\n')
                         print()
 
+#Runs the program
 program()
+	
 
